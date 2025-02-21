c73fdf5e6a74e120682d20075625ce5484972145
Anton <158073528+AntonAbabii@users.noreply.github.com>
2025-02-21 12:40:04 +0300

    Add files via upload

---
+++ Anton&Danik/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anton_Danik
+{
+    internal class Auto
+    {
+       
+        string brand;
+        string color;
+        double skor;
+        
+        public string Brand { get { return brand; } set { brand = value; } }
+        public string Color { get { return color; } set { color = value; } }
+        public double Skor { get { return skor; } set { if (value < 20) { skor = 20; } else if (value > 120) { skor = 120; } else { skor = value; } } }
+      
+        public Auto() { }
+        
+        public Auto(string brand, string color, double skor)
+        {
+            this.Brand = brand;
+            this.Color = color;
+            this.Skor = skor;
+        }
+       
+        public override string ToString()
+        {
+            return $"Brand = {Brand}, Color = {Color}, Skor = {Skor}";
+        }
+
+
+
+    }
+}
---
+++ Anton&Danik/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anton_Danik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto avto = new Auto();
+            Console.WriteLine(avto.ToString());
+          
+            Auto avto2 = new Auto("TOYOTA", "RED", 220);
+            Console.WriteLine(avto2.ToString());
+            
+            Console.WriteLine("Введите марку");
+            string brand = Console.ReadLine();
+            Console.WriteLine("Введите цвет");
+            string color = Console.ReadLine();
+            Console.WriteLine("Введите скорость");
+
+            double skor = double.Parse(Console.ReadLine());
+            Auto avto3 = new Auto(brand, color, skor);
+            Console.WriteLine($"{avto3.ToString()}\n");
+            Console.ReadLine();
+        }
+    }
+}
